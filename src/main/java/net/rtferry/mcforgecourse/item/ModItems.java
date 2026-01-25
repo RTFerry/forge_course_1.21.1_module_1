5cc171e211b0d116b3413a68d39dfa8bc5bd8b11
Jack <jacktmstr@gmail.com>
2026-01-25 21:32:47 +0000

    Added: - Azurite - Raw Azurite - Cosmii Goober - Creative Tabs

---
+++ src/main/java/net/rtferry/mcforgecourse/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.rtferry.mcforgecourse.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.rtferry.mcforgecourse.MCForgeCourseMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MCForgeCourseMod.MOD_ID);
+
+    public static final RegistryObject<Item> AZURITE = ITEMS.register("azurite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_AZURITE = ITEMS.register("raw_azurite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> COSMII_GOOBER = ITEMS.register("cosmii_goober",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+           ITEMS.register(eventBus);
+    }
+}
