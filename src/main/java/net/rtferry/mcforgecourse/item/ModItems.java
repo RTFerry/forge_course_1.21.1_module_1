1765e7e90a57183fd5912d7bf72e2827b2013ee3
Jack <jacktmstr@gmail.com>
2026-01-26 16:06:08 +0000

    Added Chainsaw

--- src/main/java/net/rtferry/mcforgecourse/item/ModItems.java
+++ src/main/java/net/rtferry/mcforgecourse/item/ModItems.java
@@ -6,6 +6,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.rtferry.mcforgecourse.MCForgeCourseMod;
+import net.rtferry.mcforgecourse.item.custom.ChainsawItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -15,6 +16,10 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RAW_AZURITE = ITEMS.register("raw_azurite",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CHAINSAW = ITEMS.register("chainsaw",
+            () -> new ChainsawItem(new Item.Properties().durability(100)));
+
+
     public static final RegistryObject<Item> COSMII_GOOBER = ITEMS.register("cosmii_goober",
             () -> new Item(new Item.Properties()));
 
